56d0db753c062c39c1dcaa15219104e99d469901
7k7kpro <51160453+7k7kpro@users.noreply.github.com>
2020-11-12 10:54:52 +0700

    Add files via upload

---
+++ Bài tập 1/like1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+// khai bao bien num kieu int 
+int main(void) {
+	int num;
+//khoi tao gia tri bien num
+	num = 5;
+//hien thi bien num ra man hinh	
+  printf("bien num co gia tri: %d\n",num);
+//khai bao bien x kieu char 
+  char c;
+//khoi tao gia tri cho bien c
+  c = 'a';
+//hien thi bien ca ra man hinh
+printf("bien c co gia tri : %c", c);
+//hien thi dong: Bien num = va bien c = a
+  getchar();
+printf("bien num = %d va bien c = %c\n", num , c);
+  return 0;
+}
