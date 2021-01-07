0a5b926d37635f9149ed0555e5f79496242ad276
tiendat <51160453+nguyentiendat77@users.noreply.github.com>
2021-01-07 10:27:23 +0700

    Add files via upload

---
+++ Bài tập 3/kt_01.c
@@ -0,0 +1,20 @@
+#include <stdio.h> 
+int main(){
+	printf("\n====BANG CUU CHUONG=====\n");
+	printf("BANG2 \n");
+	printf(" 2 x 1 = 2\n");  
+	printf(" 2 x 2 = 4\n"); 
+	printf(" 2 x 3 = 6\n"); 
+    printf("BANG3 \n");
+    printf(" 3 x 1 = 3\n");  
+    printf(" 3 x 2 = 6\n");  
+   	printf(" 3 x 4 = 12\n");  
+    printf("BANG4 \n");
+    printf(" 4 x 1 = 4\n");
+    printf(" 4 x 2 = 8\n");
+    printf(" 4 x 3 = 12\n");
+    
+
+    
+}
+
